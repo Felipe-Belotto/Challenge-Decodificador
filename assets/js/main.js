69d6b5d289145b2d2bee954d1ed3341c20260f2e
Felipe-Belotto <felipeeduardo817@gmail.com>
2023-04-14 19:00:37 -0300

    aplicando criptografia

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,13 @@ let hide = (referencia) => {referencia.style.display="none";}
 
 const botaoCriptografar = document.getElementById("botao-criptografar")
 const botaoDescriptografar = document.getElementById("botao-descriptografar")
+const botaoCopiar = document.getElementById("botao-copiar")
 
 const inputTexto = document.getElementById ("input-texto")
-const resposta = document.getElementById("resposta")
+
+
+const boxTextoResposta = document.querySelector(".box-textoResposta")
+const textoResposta = document.getElementById("textoResposta")
 const respostaSemTexto = document.getElementById("respostaSemTexto")
 
 botaoCriptografar.addEventListener("click", criptografar)
@@ -19,9 +23,26 @@ inputTexto.addEventListener("click", () => {
 
 function criptografar (){
     hide(respostaSemTexto)
+    botaoCopiar.classList.remove("hide");
+    codificaLetras()
 }
 
 function descriptografar(){
     hide(respostaSemTexto)
+}
+
+function codificaLetras () {
+    let textoInput = inputTexto.value
+
+    if (/[aeiou]/.test(textoInput)){
+        textoInput = textoInput.replace(/a/g, "ai")
+        textoInput = textoInput.replace(/e/g, "enter")
+        textoInput = textoInput.replace(/i/g, "imes")
+        textoInput = textoInput.replace(/o/g, "ober")
+        textoInput = textoInput.replace(/u/g, "ufat")
+    }
+
+    textoResposta.innerText = textoInput
+    boxTextoResposta.style.display="flex";
 }
 
