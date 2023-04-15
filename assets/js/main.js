1c153d10b7c61d26903bc3a66fc1e53c4ccf62bb
Felipe-Belotto <felipeeduardo817@gmail.com>
2023-04-14 22:37:05 -0300

    IMPORTANTE (AlteraçãoQue DeixouOcódigo Funcional)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,71 +1,55 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const botaoCriptografar = document.getElementById("botao-criptografar");
+    const botaoDescriptografar = document.getElementById("botao-descriptografar");
+    const botaoCopiar = document.getElementById("botao-copiar");
+  
+    const inputTexto = document.getElementById("input-texto");
+    const textoResposta = document.getElementById("textoResposta");
+    const respostaSemTexto = document.getElementById("respostaSemTexto");
+    const areaTextoResposta = document.getElementById("box-textoResposta")
+  
+    const algoritmo = [
+      ["e", "enter"],
+      ["i", "imes"],
+      ["a", "ai"],
+      ["o", "ober"],
+      ["u", "ufat"],
+    ];
+  
+    const mostrar = (texto) => {
+      respostaSemTexto.style.display="none"
+      botaoCopiar.style.display="block"
+      areaTextoResposta.style.display="flex"
+      textoResposta.value = texto;
+    };
+  
+    const copiar = (valor) => {
+      navigator.clipboard.writeText(valor);
+    };
 
-let hide = (referencia) => {referencia.style.display="none";}
-
-const botaoCriptografar = document.getElementById("botao-criptografar")
-const botaoDescriptografar = document.getElementById("botao-descriptografar")
-const botaoCopiar = document.getElementById("botao-copiar")
-let copiar = (valor) => {botaoCopiar.addEventListener("click", () => {navigator.clipboard.writeText(valor)})}
-
-const inputTexto = document.getElementById ("input-texto")
-
-
-const boxTextoResposta = document.querySelector(".box-textoResposta")
-const textoResposta = document.getElementById("textoResposta")
-const respostaSemTexto = document.getElementById("respostaSemTexto")
-
-botaoCriptografar.addEventListener("click", criptografar)
-botaoDescriptografar.addEventListener("click", descriptografar)
-
-inputTexto.addEventListener("click", () => {
+    inputTexto.addEventListener("click", () => {
     if(inputTexto.value == "Digite seu texto"){
-    inputTexto.value ="";
+        inputTexto.value ="";
     }
+    else {inputTexto.value = inputTexto.value}
 })
-
-function criptografar (){
-    hide(respostaSemTexto)
-    botaoCopiar.classList.remove("hide");
-    codificaLetras()
-}
-
-function descriptografar(){
-    hide(respostaSemTexto)
-    botaoCopiar.classList.remove("hide");
-    descodificaLetras()
-}
-
-function codificaLetras () {
-    let textoInput = inputTexto.value
-
-    if (/[aeiou]/.test(textoInput)){
-        textoInput = textoInput.replace(/a/g, "ai")
-        textoInput = textoInput.replace(/e/g, "enter")
-        textoInput = textoInput.replace(/i/g, "imes")
-        textoInput = textoInput.replace(/o/g, "ober")
-        textoInput = textoInput.replace(/u/g, "ufat")
-    }
-
-    textoResposta.innerText = textoInput
-    boxTextoResposta.style.display="flex";
-    botaoCopiar.addEventListener("click", copiar(textoInput))
-}
-
-function descodificaLetras(){
-    let textoInput = inputTexto.value
-    
-    if (/[aeiou]/.test(textoInput)){
-        textoInput = textoInput.replace(/ai/g, "a");
-        textoInput = textoInput.replace(/enter/g, "e");
-        textoInput = textoInput.replace(/imes/g, "i");
-        textoInput = textoInput.replace(/ober/g, "o");
-        textoInput = textoInput.replace(/ufat/g, "u");
-    }
-
-    textoResposta.innerText = textoInput;
-    boxTextoResposta.style.display="flex"; 
-    botaoCopiar.addEventListener("click", copiar(textoInput)) 
-}
-
-
-
-
+  
+    botaoCriptografar.addEventListener("click", () => {
+      let textoCriptografado = inputTexto.value.toLowerCase();
+      for (let i = 0; i < algoritmo.length; i++) {
+        textoCriptografado = textoCriptografado.replaceAll(algoritmo[i][0], algoritmo[i][1]);
+      }
+      mostrar(textoCriptografado);
+      copiar(textoCriptografado);
+    });
+  
+    botaoDescriptografar.addEventListener("click", () => {
+      let textoDescriptografado = inputTexto.value.toLowerCase();
+      for (let i = 0; i < algoritmo.length; i++) {
+        textoDescriptografado = textoDescriptografado.replaceAll(algoritmo[i][1], algoritmo[i][0]);
+      }
+      mostrar(textoDescriptografado);
+      copiar(textoDescriptografado);
+    });
+  });
+  
